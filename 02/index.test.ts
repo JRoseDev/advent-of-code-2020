833b8e9d8e9482dd2158741e7bc01987c186c6a2
Joshua Rose <joshua.rose@outlook.com>
2020-12-02 21:16:05 +0000

    Day 2 Part 2

--- 02/index.test.ts
+++ 02/index.test.ts
@@ -1,10 +1,16 @@
-import { findValidPasswords, solvePart1 } from ".";
+import {
+    findValidPasswords,
+    schemeA,
+    schemeB,
+    solvePart1,
+    solvePart2,
+} from ".";
 
 describe("02", () => {
     describe("Part 1 - Find the passwords that are valid", () => {
         describe("findValidPasswords", () => {
             it("excludes invalid passwords", () => {
-                const actual = findValidPasswords([
+                const actual = findValidPasswords(schemeA, [
                     "2-6 w: wkwwwfwwpv",
                     "14-15 v: hvhvlhvvvwxvdvscdpvg",
                 ]);
@@ -25,6 +31,33 @@ describe("02", () => {
                 const actual = solvePart1();
 
                 expect(actual).toEqual(536);
+            });
+        });
+    });
+    describe("Part 2 - Find the passwords that are valid under the other scheme", () => {
+        describe("findValidPasswords", () => {
+            it("excludes invalid passwords", () => {
+                const actual = findValidPasswords(schemeB, [
+                    "2-6 w: wkwwwwfwpv",
+                    "14-15 v: hvhvlhvvvwxvdxscdpxg",
+                ]);
+
+                expect(actual).toEqual([
+                    {
+                        character: "w",
+                        firstPosition: 1,
+                        password: "wkwwwwfwpv",
+                        secondPosition: 5,
+                    },
+                ]);
+            });
+        });
+
+        describe("solvePart2", () => {
+            it("solves the puzzle!", () => {
+                const actual = solvePart2();
+
+                expect(actual).toEqual(558);
             });
         });
     });
--- 02/index.ts
+++ 02/index.ts
@@ -1,35 +1,87 @@
 import { readFileSync } from "fs";
 
 const input = readFileSync("./02/input.txt").toString().split("\r\n");
-interface ParsedRow {
+interface SchemeARow {
     minOcurrences: number;
     maxOcurrences: number;
     character: string;
     password: string;
 }
 
-const rowRegEx = /(?<minOcurrences>\d+)-(?<maxOcurrences>\d+) (?<character>\w): (?<password>.*$)/iu;
+interface SchemeBRow {
+    firstPosition: number;
+    secondPosition: number;
+    character: string;
+    password: string;
+}
 
-const isValid = (row: ParsedRow) => {
-    const count =
-        row.password.match(new RegExp(row.character, "g"))?.length || 0;
+interface Scheme<T> {
+    parse: (row: string) => T;
+    validate: (parsedRow: T) => boolean;
+}
 
-    return count >= row.minOcurrences && count <= row.maxOcurrences;
+const rowRegEx = /(?<firstNumber>\d+)-(?<secondNumber>\d+) (?<character>\w): (?<password>.*$)/iu;
+
+export const schemeA: Scheme<SchemeARow> = {
+    parse: (row: string): SchemeARow => {
+        const {
+            firstNumber: minOcurrences,
+            secondNumber: maxOcurrences,
+            character,
+            password,
+        } = rowRegEx.exec(row)?.groups || {};
+
+        return {
+            minOcurrences: Number.parseInt(minOcurrences),
+            maxOcurrences: Number.parseInt(maxOcurrences),
+            character,
+            password,
+        };
+    },
+    validate: (row: SchemeARow) => {
+        const count =
+            row.password.match(new RegExp(row.character, "g"))?.length || 0;
+
+        return count >= row.minOcurrences && count <= row.maxOcurrences;
+    },
 };
 
-const parseRow = (row: string): ParsedRow => {
-    const { minOcurrences, maxOcurrences, character, password } =
-        rowRegEx.exec(row)?.groups || {};
+export const schemeB: Scheme<SchemeBRow> = {
+    parse: (row: string): SchemeBRow => {
+        const {
+            firstNumber: firstPosition,
+            secondNumber: secondPosition,
+            character,
+            password,
+        } = rowRegEx.exec(row)?.groups || {};
 
-    return {
-        minOcurrences: Number.parseInt(minOcurrences),
-        maxOcurrences: Number.parseInt(maxOcurrences),
-        character,
-        password,
-    };
+        return {
+            firstPosition: Number.parseInt(firstPosition) - 1,
+            secondPosition: Number.parseInt(secondPosition) - 1,
+            character,
+            password,
+        };
+    },
+    validate: (row: SchemeBRow) => {
+        const {
+            [row.firstPosition]: firstCharacter,
+            [row.secondPosition]: secondCharacter,
+        } = row.password;
+
+        const firstMatches = firstCharacter === row.character;
+        const secondMatches = secondCharacter === row.character;
+
+        return (
+            (firstMatches && !secondMatches) || (secondMatches && !firstMatches)
+        );
+    },
 };
 
-export const findValidPasswords = (row: string[]) =>
-    row.map(parseRow).filter(isValid);
+export const findValidPasswords = <T extends SchemeARow | SchemeBRow>(
+    scheme: Scheme<T>,
+    rows: string[]
+) => rows.map(scheme.parse).filter(scheme.validate);
 
-export const solvePart1 = () => findValidPasswords(input).length;
+export const solvePart1 = () => findValidPasswords(schemeA, input).length;
+
+export const solvePart2 = () => findValidPasswords(schemeB, input).length;
