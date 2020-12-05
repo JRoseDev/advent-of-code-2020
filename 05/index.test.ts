58fcab7faf3c4c156997f18ee78edeab64ff5397
Joshua Rose <joshua.rose@squaredup.com>
2020-12-05 21:22:58 +0000

    Day 5 Part 1

---
+++ 05/index.test.ts
@@ -0,0 +1,46 @@
+import { findSeat, parseBoardingPass, solvePart1, toSeatId } from ".";
+
+describe("05", () => {
+    describe("Part 1 - Find the highest seat Id", () => {
+        describe("parseBoardingPass", () => {
+            it("splits row path from seat path", () => {
+                const actual = parseBoardingPass("FBFBBBFRLR");
+
+                expect(actual).toEqual({
+                    rowPath: ["F", "B", "F", "B", "B", "B", "F"],
+                    columnPath: ["R", "L", "R"]
+                });
+            });
+        });
+
+        describe("findSeat", () => {
+            it("resolves paths to indexes", () => {
+                const actual = findSeat(
+                    8,
+                    4
+                )({
+                    rowPath: ["F", "B", "F"],
+                    columnPath: ["R", "R"]
+                });
+
+                expect(actual).toEqual([2, 3]);
+            });
+        });
+
+        describe("toSeatId", () => {
+            it("converts seat location to Id", () => {
+                const actual = toSeatId([4, 12]);
+
+                expect(actual).toEqual(44);
+            });
+        });
+
+        describe("solvePart1", () => {
+            it("solves the puzzle!", () => {
+                const actual = solvePart1();
+
+                expect(actual).toEqual(978);
+            });
+        });
+    });
+});
---
+++ 05/index.ts
@@ -0,0 +1,52 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("./05/input.txt").toString().split("\r\n");
+
+type RowPath = ("F" | "B")[];
+type ColumnPath = ("R" | "L")[];
+
+interface BoardingPass {
+    rowPath: RowPath;
+    columnPath: ColumnPath;
+}
+
+type Seat = [row: number, column: number];
+
+const toCharArray = <T extends string>(chars: T[], count: number, s: string): T[] =>
+    (new RegExp(`^[${chars}]{${count}}`).exec(s)?.[0]?.split("") as T[]) || [];
+
+export const parseBoardingPass = (s: string): BoardingPass => ({
+    rowPath: toCharArray(["F", "B"], 7, s),
+    columnPath: toCharArray(["L", "R"], 3, s.slice(7))
+});
+
+const resolvePath = <T extends string[]>(
+    isLeft: (x: string) => boolean,
+    blockSize: number,
+    [p, ...path]: T,
+    rowIndex: number = 0
+): number => {
+    if (blockSize === 1) {
+        return rowIndex;
+    }
+
+    return isLeft(p)
+        ? resolvePath(isLeft, blockSize / 2, path, rowIndex)
+        : resolvePath(isLeft, blockSize / 2, path, rowIndex + blockSize / 2);
+};
+
+export const findSeat = (rowCount: number, columnCount: number) => (
+    boardingPass: BoardingPass
+): Seat => [
+    resolvePath((s) => s === "F", rowCount, boardingPass.rowPath),
+    resolvePath((s) => s === "L", columnCount, boardingPass.columnPath)
+];
+
+export const toSeatId = ([row, column]: Seat): number => row * 8 + column;
+
+export const solvePart1 = () =>
+    input
+        .map(parseBoardingPass)
+        .map(findSeat(128, 8))
+        .map(toSeatId)
+        .reduce((max, id) => (id > max ? id : max));
