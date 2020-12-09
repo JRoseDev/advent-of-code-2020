7201f0aec9314d09aa2a7d539e0832fa1044219e
Joshua Rose <joshua.rose@outlook.com>
2020-12-09 20:38:10 +0000

    Day 6 Part 2

--- 06/index.test.ts
+++ 06/index.test.ts
@@ -1,12 +1,18 @@
-import { groupAnswers, solvePart1 } from ".";
+import { countAnswers, groupAnswers, solvePart1, solvePart2 } from ".";
 
 describe("06", () => {
     describe("Part 1 - Count the questions answered with yes", () => {
         describe("groupAnswers", () => {
             it("groups answers from each group", () => {
-                const actual = groupAnswers(`qzbw\r\nqez\r\n\r\nxged\r\nfxgp`);
+                const actual = groupAnswers(
+                    `qzbw\r\nqez\r\n\r\nxged\r\nfxgp\r\n\r\none`
+                );
 
-                expect(actual).toEqual(["qzbwqez", "xgedfxgp"]);
+                expect(actual).toEqual([
+                    { answers: "qzbwqez", memberCount: 2 },
+                    { answers: "xgedfxgp", memberCount: 2 },
+                    { answers: "one", memberCount: 1 },
+                ]);
             });
         });
 
@@ -15,6 +21,31 @@ describe("06", () => {
                 const actual = solvePart1();
 
                 expect(actual).toEqual(6742);
+            });
+        });
+    });
+
+    describe("Part 2 - Count the questions everyone answered with yes", () => {
+        describe("countAnswers", () => {
+            it("counts the instances of each answer", () => {
+                const actual = countAnswers("abcabcdefa");
+
+                expect(actual).toEqual({
+                    a: 3,
+                    b: 2,
+                    c: 2,
+                    d: 1,
+                    e: 1,
+                    f: 1,
+                });
+            });
+        });
+
+        describe("solvePart2", () => {
+            it("solves the puzzle!", () => {
+                const actual = solvePart2();
+
+                expect(actual).toEqual(727);
             });
         });
     });
--- 06/index.ts
+++ 06/index.ts
@@ -2,11 +2,38 @@ import { readFileSync } from "fs";
 
 const input = readFileSync("./06/input.txt").toString();
 
-export const groupAnswers = (input: string) =>
-    input.split("\r\n\r\n").map((s) => s.replace(/\r\n/g, ""));
+interface GroupAnswers {
+    answers: string;
+    memberCount: number;
+}
+
+export const groupAnswers = (input: string): GroupAnswers[] =>
+    input.split("\r\n\r\n").map((s) => {
+        const memberCount = (s.match(/\r\n/giu)?.length || 0) + 1;
+
+        return { answers: s.replace(/\r\n/g, ""), memberCount };
+    });
+
+export const countAnswers = (answers: string): Record<string, number> =>
+    Array.prototype.reduce.bind(answers)(
+        (acc: Record<string, number>, curr: string) => (
+            (acc[curr] = (acc[curr] || 0) + 1), acc
+        ),
+        {}
+    );
 
 export const solvePart1 = () =>
     groupAnswers(input)
-        .map((s) => new Set(s))
+        .map(({ answers }) => new Set(answers))
         .reduce((acc, s) => acc + s.size, 0);
 
+export const solvePart2 = () =>
+    groupAnswers(input)
+        .map(({ answers, memberCount }) => {
+            const answerCounts = countAnswers(answers);
+
+            return Object.keys(answerCounts).filter(
+                (k) => answerCounts[k] === memberCount
+            );
+        })
+        .reduce((acc, a) => acc + a.length, 0);
