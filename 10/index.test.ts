272c348fa7f1ca28bade287869fd637f07dc5f79
Joshua Rose <joshua.rose@outlook.com>
2020-12-17 21:35:43 +0000

    Day 10 Part 2

--- 10/index.test.ts
+++ 10/index.test.ts
@@ -1,4 +1,4 @@
-import { solvePart1 } from ".";
+import { solvePart1, solvePart2 } from ".";
 
 describe("10", () => {
     describe("Part 1 - Multiply the number of differences of 1 by the number of differences of 3", () => {
@@ -7,6 +7,54 @@ describe("10", () => {
                 const actual = solvePart1();
 
                 expect(actual).toEqual(3034);
+            });
+        });
+    });
+
+    describe("Part 2 - Count the number of distinct orders possible", () => {
+        describe("solvePart2", () => {
+            it("solves the example input", () => {
+                const actual = solvePart2([
+                    28,
+                    33,
+                    18,
+                    42,
+                    31,
+                    14,
+                    46,
+                    20,
+                    48,
+                    47,
+                    24,
+                    23,
+                    49,
+                    45,
+                    19,
+                    38,
+                    39,
+                    11,
+                    1,
+                    32,
+                    25,
+                    35,
+                    8,
+                    17,
+                    7,
+                    9,
+                    4,
+                    2,
+                    34,
+                    10,
+                    3,
+                ]);
+
+                expect(actual).toEqual(19208);
+            });
+
+            it("solves the puzzle!", () => {
+                const actual = solvePart2();
+
+                expect(actual).toEqual(259172170858496);
             });
         });
     });
--- 10/index.ts
+++ 10/index.ts
@@ -26,3 +26,27 @@ export const solvePart1 = () => {
 
     return diffs["1Diffs"] * (diffs["3Diffs"] + 1);
 };
+
+const countBranches = (numbers: number[]): number => {
+    const [max] = numbers;
+    const cache: Map<number, number> = new Map([[max, 1]]);
+
+    for (const start of numbers) {
+        const canReach = numbers.filter((n) => n > start && n - start <= 3);
+
+        canReach.forEach((n) => {
+            cache.set(start, (cache.get(start) || 0) + (cache.get(n) || 0));
+        });
+    }
+
+    return cache.get(0) || 0;
+};
+
+export const solvePart2 = (n: number[] = input) => {
+    const descending = n.sort((a, b) => b - a);
+    const [max] = descending;
+
+    const withStartAndEnd = [max + 3, ...descending, 0];
+
+    return countBranches(withStartAndEnd);
+};
